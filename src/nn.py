e249a26423917f8c557cd2e91884fb6ae67646e8
javier <javier.rojas02@alumnos.uach.cl>
2022-11-06 18:22:57 -0300

    Create nn.py

---
+++ src/nn.py
@@ -0,0 +1,15 @@
+import torch 
+class my_nn(torch.nn.Module):
+    def __init__(self):
+        super(my_nn,self).__init__()
+        self.linear1= torch.nn.Linear(28*28,256)
+        self.linear2= torch.nn.Linear(256,64)
+        self.linear3= torch.nn.Linear(64,32)
+        self.linear4= torch.nn.Linear(32,10)
+        self.soft = torch.nn.Softmax()
+        self.relu= torch.nn.ReLU()
+    def forward(self,x):
+        x=self.relu(self.linear1(x))
+        x=self.relu(self.linear2(x))
+        x=self.relu(self.linear3(x))
+        return self.soft(self.linear4(x))
